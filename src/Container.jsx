5cdc9c85bcdb8c580ad33c3a2a6ba4d75cc71ab8
siddu4236 <siddujcl99@gmail.com>
2025-03-29 23:01:24 +0530

    fc1

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,7 +1,7 @@
 
 import './Container.css'
 import Card1 from './containercard/Card1/Card1';
-
+import Card2 from './containercard/Card2/Card2';
 
 
 
@@ -9,6 +9,7 @@ export default function container(){
     return(
         <div className="main-container">
             <Card1 />
+            <div className="moveleft1"><Card2 /></div>
         </div>
         
     );
