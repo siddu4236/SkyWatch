881ebc15eca93673c599446003acb379ff20d5d6
siddu4236 <siddujcl99@gmail.com>
2025-04-08 17:40:44 +0530

    fc11

--- src/containercard/Card1/Card1.jsx
+++ src/containercard/Card1/Card1.jsx
@@ -1,6 +1,10 @@
 import "./Card1.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import cloudy from "../gifs/cloudy.gif";
+import temp from "../gifs/temp2.gif";
+import humid from "../gifs/humidity.gif";
+import pressure from "../gifs/at2.png";
 
 export default function Card1() {
   return (
@@ -9,25 +13,42 @@ export default function Card1() {
         <p className="now-now">Now</p>
         <div className="clouds">
           <p>
+            <img className="temp" style={{ height: "50px" }} src={temp} alt="Temperature" />
             20<span>&#8451;</span>
-            <img src={cloudy} alt="Cloudy Weather" />
+            <img className="cloudy" src={cloudy} alt="Cloudy Weather" />
           </p>
         </div>
       </div>
-      <div className="Typeofweather">
-        <p className="Type-weather">Haze</p>
-        <div className="calen">
-          <i class="fa-regular fa-calendar"></i>
-          <span className="date">Thurday 2, Mar</span>
-        </div>
-        <div className="loc">
-        <i class="fa-solid fa-location-dot"></i>
-          <span className="area">New Delhi IN</span>
-        </div>
+      
+      <div className="hum-press">
+      <div className="heading">
+        <span className="humidd">Humidity</span>
+        <span className="presss">Pressure</span>
+      </div>
+      <div className="humid"> 
+      
+      <img className="humidimg" src={humid} alt="Cloudy Weather" />
+      <span>82 %</span>
       </div>
       
-      
+      <div className="press"> 
+        
+      <img className="pressure" src={pressure} alt="Cloudy Weather" />
+      <span>82 %</span>
+      </div>
+      </div>
+      <div className="Typeofweather">
+        <p className="Type-weather">Feels Like Haze</p>
+        <div className="calen">
+          <FontAwesomeIcon icon={faCalendar} />
+          <span className="date">Thursday 2, Mar</span>
+        </div>
+        <div className="loc">
+          <FontAwesomeIcon icon={faLocationDot} />
+          <span className="area">New Delhi, IN</span>
+        </div>
+      </div>
     </div>
   );
 }
-<p className="Type-weather">Haze</p>;
+
--- src/containercard/Card2/Card2.jsx
+++ src/containercard/Card2/Card2.jsx
@@ -5,27 +5,13 @@ import cloudy from "../gifs/cloudy.gif";
 export default function Card1() {
   return (
     <div className="wholeCard1">
-      <div className="now">
-        <p className="now-now">Now</p>
-        <div className="clouds">
-          <p>
-            20<span>&#8451;</span>
-            <img src={cloudy} alt="Cloudy Weather" />
-          </p>
-        </div>
+      <div className="today">
+      <p className="high">Todays Highlights</p>
+      </div>   
+      <div className="air-quality">
+        <p className="air-qua">Air Quality Index</p>
+        
       </div>
-      <div className="Typeofweather">
-        <p className="Type-weather">Haze</p>
-        <div className="calen">
-          <i class="fa-regular fa-calendar"></i>
-          <span className="date">Thurday 2, Mar</span>
-        </div>
-        <div className="loc">
-        <i class="fa-solid fa-location-dot"></i>
-          <span className="area">New Delhi IN</span>
-        </div>
-      </div>
-      
       
     </div>
   );
